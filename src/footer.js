c3ce9f5973feeed182676665ee7688e507b58e6d
samsalgado <samueljuansalgado@gmail.com>
2024-09-10 22:01:27 -0400

    update

--- src/footer.js
+++ src/footer.js
@@ -11,14 +11,14 @@ const Footer = () => {
   return (
 <div className="footer-container">
 <div className='socials'>
+<a href='https://btcpay0.voltageapp.io/apps/WYdgLYf994wmcnonsQdN1UrWZ6K/crowdfund'>
+            <img src={suriname} alt='' width={80} height={80}  />
+        </a>
         <a href='https://twitter.com/ttf123457'>
             <img src={x} alt='' width={80} height={80} />
         </a>
         <a href='https://www.youtube.com/channel/UCviB2N6lC9aJu0eSw6FijWQ'>
             <img src={youtube} alt='' width={80} height={80}  />
-        </a>
-        <a href='https://btcpay0.voltageapp.io/apps/WYdgLYf994wmcnonsQdN1UrWZ6K/crowdfund'>
-            <img src={suriname} alt='' width={80} height={80}  />
         </a>
         </div>
 
