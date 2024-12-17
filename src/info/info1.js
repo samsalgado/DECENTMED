5a1ec6f5171c32f0f6f0906cff5922cc08dd97fb
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2024-12-16 22:48:46 -0300

    Update info1.js

--- src/info/info1.js
+++ src/info/info1.js
@@ -11,7 +11,7 @@ const Info1 = () => {
 
       <iframe
             width="100%"
-            height="700px"
+            height={315}
             src="https://www.youtube.com/embed/cJIHng7oaWM?si=zzIkenFP5H3l6ahb"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
