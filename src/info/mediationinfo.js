4d3e2108a9d07a1c7dfb4a6e6cdd57b73bf86dc7
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-01-09 23:39:52 +0900

    Update mediationinfo.js

--- src/info/mediationinfo.js
+++ src/info/mediationinfo.js
@@ -8,6 +8,7 @@ const MeditationInfo = () => {
 <iframe width="100%" height={500} src="https://www.youtube.com/embed/cQ-pEQZrVvw" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
 
 <Button variant="primary" href="https://sanjivana.com/" target="_blank">Visit Serenity Meditations</Button>  {/* Added Bootstrap Button */}
+<Button variant="primary" href="mailto:serenitymeditation2021@gmail.com" target="_blank">Contact</Button>  {/* Added Bootstrap Button */}
     <br></br>
     <div className='pr'>
         <h3>Benefits of Meditation</h3>
