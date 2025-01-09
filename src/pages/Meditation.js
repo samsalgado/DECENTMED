bb1680cbdfe3f13b37e3c2a571884796c22fc62e
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-01-09 09:37:49 +0900

    Create Meditation.js

---
+++ src/pages/Meditation.js
@@ -0,0 +1,25 @@
+import '../App.css';
+import Topbar from './topbar';
+import Footer from '../footer';
+import MeditationInfo from '../info/meditationinfo';
+import { Helmet } from 'react-helmet'; // Import Helmet
+export function Meditation({t}) {
+    return (
+        <>
+                <Helmet>  {/* Add Helmet component */}
+        <title>{t("Sleep Meditation")}</title>  
+        <meta name="description" content={t("Discover the transformative power of Meditation! Sleep meditation and more; how meditation can help you heal. Learn more.")} />
+      </Helmet>
+
+        <header>
+        <Topbar />
+        </header>
+            <MeditationInfo />
+       <br></br>
+       
+        <footer>
+            <Footer />
+        </footer>
+    </>
+    )
+}
