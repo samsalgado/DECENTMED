dfcd5125edc1b81e79408eb2bad70c88ae5eafd5
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-01-28 13:57:18 -0300

    Update hoxsey.js

--- src/pages/blogs/hoxsey.js
+++ src/pages/blogs/hoxsey.js
@@ -1,7 +1,7 @@
 import Topbar from "../topbar";
 import { Helmet } from "react-helmet";
 import Footer from "../../footer";
-import fenben from '../../images copy/Fenbendazole.png';
+import fenbenn from '../../images copy/Fenbendazole.png';
 import ReactPlayer from 'react-player';
 import Collapsible from 'react-collapsible';
 import starvecancer from '../../images copy/starvecancer.webp';
@@ -273,7 +273,7 @@ const Hoxsey = ({t}) => {
         <table className="collapsibles-table">
           <tbody> 
           <Collapsible trigger={<th className="collapsible-trigger">Cancer Cures</th>}>
-               <a href="/fenbendazole"> <img src={fenben} alt='' /> </a>
+               <a href="/fenbendazole"> <img src={fenbenn} alt='' /> </a>
                <a href="/ivermectin"> <img src={IVERMECTINCANCER} alt="img" /> </a>
                <a href="https://www.howtostarvecancer.com/"> <img src={starvecancer} alt="img" style={{ width: '200px', height: 'auto' }} /> </a>
             </Collapsible>
