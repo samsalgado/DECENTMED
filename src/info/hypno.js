1dd85aabfbbfaff529c1a2bd94b03e69995c85bb
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-01-28 11:40:31 -0300

    Update hypno.js

--- src/info/hypno.js
+++ src/info/hypno.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './acu.css'; 
-import ReactPlayer from 'react-player';
 import { Button } from'react-bootstrap';
 import Benefits from "./benefits";
 import Modalities from "./modalities";
@@ -8,12 +7,17 @@ const HYPNO = () => {
     return (
         <div className="container">
             <h1>Hypnotherapy</h1>
-      <ReactPlayer
-  url="https://rumble.com/embed/v6c1np4/?pub=4hu51y"
-  width="100%"
-  height="400px"
-  controls={true}
-/>
+    <div>
+  <iframe
+    className="rumble"
+    width="100%"
+    height="400px"
+    src="https://rumble.com/embed/v6c1np4/?pub=4hu51y"
+    frameBorder="0"
+    allowFullScreen
+    title="Rumble video player"
+  ></iframe>
+</div>
       <Button variant="primary" href="https://www.green-hypnotherapy.com/" target="_blank">Visit Green Hypnotherapy</Button>  {/* Added Bootstrap Button */}
         <br></br>
         <Benefits />
