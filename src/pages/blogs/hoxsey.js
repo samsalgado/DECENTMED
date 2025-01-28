5be45d50da07590ce2d722ef9787c7c14169d305
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-01-27 22:27:02 -0300

    Create hoxsey.js

---
+++ src/pages/blogs/hoxsey.js
@@ -0,0 +1,259 @@
+import Topbar from "../topbar";
+import { Helmet } from "react-helmet";
+import Footer from "../../footer";
+import fenben from '../../images copy/IMG_6431.jpeg';
+import fenben2 from '../../images copy/IMG_6433.jpeg';
+import fenben3 from '../../images copy/IMG_6434.jpeg';
+import fenben4 from '../../images copy/IMG_6435.jpeg';
+import fenben5 from '../../images copy/IMG_6436.jpeg';
+import fenben6 from '../../images copy/IMG_6437.jpeg';
+import fenben7 from '../../images copy/IMG_6438.jpeg';
+import fenben8 from '../../images copy/IMG_6439.jpeg';
+import fenben9 from '../../images copy/IMG_6440.jpeg';
+import fenben10 from '../../images copy/IMG_6441.jpeg';
+import fenben11 from '../../images copy/IMG_6442.jpeg';
+import fenben12 from '../../images copy/IMG_6443.jpeg';
+import fenben13 from '../../images copy/IMG_6445.jpeg';
+import ivermectin14 from '../../images copy/IMG_6444.jpeg';
+import ivermectin15 from '../../images copy/IMG_6446.jpeg';
+import ivermectin16 from '../../images copy/IMG_6447.jpeg';
+import ivermectin17 from '../../images copy/IMG_6448.jpeg';
+
+import hoxsey18 from '../../images copy/IMG_6449.jpeg';
+
+import hoxsey19 from '../../images copy/IMG_6450.jpeg';
+
+import hoxsey20 from '../../images copy/IMG_6451.jpeg';
+
+import hoxsey22 from '../../images copy/IMG_6452.jpeg';
+
+import hoxsey24 from '../../images copy/IMG_6453.jpeg';
+
+import hoxsey24 from '../../images copy/IMG_6454.jpeg';
+
+import hoxsey23 from '../../images copy/IMG_6455.jpeg';
+
+import hoxsey24 from '../../images copy/IMG_6456.png';
+import hoxsey25 from '../../imagescopy/IMG_6457.jpeg
+
+import hoxsey26 from '../../imagescopy/IMG_6458.jpeg
+
+import hoxsey27 from '../../imagescopy/IMG_6459.jpeg
+
+import hoxsey28 from '../../images copy/IMG_6460.jpeg';
+
+import hoxsey29 from '../../images copy/IMG_6461.jpeg';
+
+import hoxsey30 from '../../images copy/IMG_6462.jpeg';
+
+import hoxsey31 from '../../images copy/IMG_6462.jpeg';
+
+import hoxsey32 from '../../images copy/IMG_6463.jpeg';
+
+import hoxsey33 from '../../images copy/IMG_6464.jpeg';
+
+import hoxsey34 from '../../images copy/IMG_6465.jpeg';
+
+import hoxsey35 from '../../images copy/IMG_6466.jpeg';
+
+import hoxsey36 from '../../images copy/IMG_6467.jpeg';
+
+import hoxsey37 from '../../images copy/IMG_6468jpeg';
+
+import hoxsey38 from '../../images copy/IMG_6469.jpeg';
+
+import hoxsey39 from '../../images copy/IMG_6470.jpeg';
+
+import hoxsey40 from '../../images copy/IMG_6471.jpeg';
+
+import hoxsey41 from '../../images copy/IMG_6472.jpeg';
+
+import "../../App.css";
+const Hoxsey = ({t}) => {
+  return (
+    <>
+      <Helmet>
+        <title>{t("You Don’t Have to Die")}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={t("Documenting the efficacy of Cancer treatments. You Don’t Have to Die by Harry Hoxsey outlines history and proof of his cancer treatment and its efficacy.")} />
+      </Helmet>
+      <header>
+        <Topbar />
+      </header>
+      <div className='container'>
+      <h1 style={{ textDecoration: 'underline', textAlign: 'center', margin: '20px 0' }}>{t("Harry Hoxsey: You Don’t Have to Die")}
+      </h1>
+      <div className='container'>
+<iframe width="100%" height="600px" src="https://www.youtube.com/embed/MlrhNMaYLmk" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+</div>
+      </div>
+      <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+          <div className='container'>
+          <p>
+          <img className='gridimag' src={fenben} alt="logo" />
+        </p>
+        <p>
+        <img className='gridimag' src={fenben2} alt="logo" />
+           </p>
+           <p>
+           <img className='gridimag' src={fenben3} alt="logo" />
+           </p>
+           <p>
+            <img className='gridimag' src={fenben4} alt="logo" />
+           </p>
+           <p>
+            <img className='gridimag' src={fenben5} alt="pic" />
+           </p>
+           <p>
+           <img className='gridimag' src={fenben6} alt="pic" />
+           </p>
+           <p>
+            <img className='gridimag' src={fenben8} alt="pic" />
+           </p>
+           <p>
+           <img className='gridimag' src={fenben7} alt="pic" />
+           </p>
+           <p>
+           <img className='gridimag' src={fenben9} alt="pic" />
+           </p>
+           <p>
+           <img className='gridimag' src={fenben10} alt="pic" />
+           </p>
+            <p>
+                <img className='gridimag' src={fenben11} alt="pic" />
+            </p>
+            <p>
+                <img className='gridimag' src={fenben12} alt="pic" />
+            </p>
+            <p>
+                <img className='gridimag' src={fenben13} alt="pic" />
+            </p>
+            <p>
+                <img className='gridimag' src={ivermectin14} alt="pic" />
+            </p>
+            <p>
+                <img className='gridimag' src={ivermectin15} alt="pic" />
+            </p>
+            <p>
+                <img className='gridimag' src={ivermectin16} alt="pic" />
+            </p>
+            <p>
+                <img className='gridimag' src={ivermectin17} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey18} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey19} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey20} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey21} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey22} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey23} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey24} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey25} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey26} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey27} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey28} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey29} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey30} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey31} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey32} alt="pic" />
+            </p>
+
+            <p>
+                <img className='gridimag' src={hoxsey33} alt="pic" />
+            </p>
+
+ <p>
+                <img className='gridimag' src={hoxsey34} alt="pic" />
+            </p>
+
+ <p>
+                <img className='gridimag' src={hoxsey35} alt="pic" />
+            </p>
+
+ <p>
+                <img className='gridimag' src={hoxsey36} alt="pic" />
+            </p>
+
+ <p>
+                <img className='gridimag' src={hoxsey37} alt="pic" />
+            </p>
+
+ <p>
+                <img className='gridimag' src={hoxsey38} alt="pic" />
+            </p>
+
+ <p>
+                <img className='gridimag' src={hoxsey39} alt="pic" />
+            </p>
+
+ <p>
+                <img className='gridimag' src={hoxsey40} alt="pic" />
+            </p>
+
+  <p>
+                <img className='gridimag' src={hoxsey41} alt="pic" />
+            </p>
+          <p>
+            <a href="https://docs.google.com/presentation/d/11ioJ2uKusayB5mbCPyp9BjtM_D49qY_CaQdf0Rk8ORg/edit">Slide Show</a>   
+           </p>
+           <p>
+            <a href="https://www.dropbox.com/scl/fi/lips9wksb1voy7wwcvi01/YouDontHaveToDie_HarryHoxsey.pdf?rlkey=95a8mmfufb4vfei6os3249aq5&st=0o8e265b&dl=0">You Don’t Have to Die Full Book</a>   
+           </p>
+
+  <p>
+            <a href="https://www.dropbox.com/scl/fi/lips9wksb1voy7wwcvi01/YouDontHaveToDie_HarryHoxsey.pdf?rlkey=95a8mmfufb4vfei6os3249aq5&st=0o8e265b&dl=0">You Don’t Have to Die Full Book</a>   
+           </p>
+<p>
+  <a href="https://youtu.be/0TGjm7wIyUk?si=bw0UwHeh2IPGJxGN">Krebiozen Full Movie</a>
+</p>
+          </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Hoxsey; // Default export
