820811948b56d8893d7d286d7c0e8441735c6509
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-02-02 11:21:11 -0300

    Update components.js

--- src/info/components.js
+++ src/info/components.js
@@ -7,7 +7,9 @@ function Components() {
             <div className="row justify-content-center">
                 <div className="col-12 col-sm-6 col-md-3 text-center mb-4">
                     <h4>Digital Business Card</h4>
-                    <p className="edu">Showcase your company with a direct link to your booking page. The practices above will take users to a scrollview of  As the project scales, there will be built-in 24 hour scheduler.</p>
+                    <p className="edu">Showcase your company with a direct link to your booking page. The practices above will take users to a scrollview of  As the project scales, there will be built-in 24 hour scheduler.
+                       Finally, all providers on the platform receive a showcase interview for your Telehealth portal.
+                         </p>
                 </div> 
                 <div className="col-12 col-sm-6 col-md-3 text-center mb-4">
                     <h4>Access to Client Databases</h4>
@@ -21,7 +23,7 @@ function Components() {
                 </div>
                 <div className="col-12 col-sm-6 col-md-3 text-center mb-4">
                     <h4>Branded Content</h4>
-                    <p className="edu">Providers get to showcase their content on our blog page. The education pages are your canvas. All content showcased on the education page will be branded to your respective practice.</p>
+                    <p className="edu">Providers get to showcase their content on our Blog page and various Education pages. The education pages are your canvas. All content showcased on the education page will be branded to your respective practice.</p>
                 </div>
             </div>
            
