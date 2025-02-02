241083383b9e1cc0cb811593cd6a0120d5d1ea04
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-02-02 11:45:04 -0300

    Create services.js

---
+++ src/info/services.js
@@ -0,0 +1,74 @@
+import React from "react";
+import '../App.css';
+
+function Services() {
+    return (
+        <div className="container-bblue">
+            <div className="row justify-content-center">
+                <div className="col-12 col-sm-6 col-md-3 text-center mb-4">
+                    <h4>Technical SEO</h4>
+                    <p className="edu">
+                        - SEO & Social Media Audits <br />
+                        - Keyword optimization <br />
+                        - Improving page speed & web performance <br />
+                        - Growing backlinks <br />
+                        - Goal: Achieve top Google rankings  
+                    </p>
+                </div> 
+
+                <div className="col-12 col-sm-6 col-md-3 text-center mb-4">
+                    <h4>Holistic Telehealth Conferences</h4>
+                    <p className="edu">
+                        - Hosting virtual & onsite holistic medicine conferences <br />
+                        - Topics: Acupuncture, Chiropractic, Nutrition, Preventive Medicine, Stem Cell Therapy, Ayurveda, Chelation, Naturopathy, Hypnotherapy, and Homeopathy <br />
+                        - Focus on education & industry promotion  
+                    </p>
+                </div> 
+
+                <div className="col-12 col-sm-6 col-md-3 text-center mb-4">
+                    <h4>Social Media Marketing</h4>
+                    <p className="edu">
+                        - Managing social media accounts <br />
+                        - Developing custom strategies for growth & engagement <br />
+                        - Enhancing online presence  
+                    </p>
+                </div>
+
+                <div className="col-12 col-sm-6 col-md-3 text-center mb-4">
+                    <h4>Website Management</h4>
+                    <p className="edu">
+                        - Custom page development <br />
+                        - Backend security & maintenance <br />
+                        - Marketing & analytics integration  
+                    </p>
+                </div>
+
+                <div className="col-12 col-sm-6 col-md-3 text-center mb-4">
+                    <h4>Web Analytics & Reporting</h4>
+                    <p className="edu">
+                        - Weekly SEO audits using SEMRush, Moz, Google Search Console, and other tools <br />
+                        - Data-driven insights to optimize performance  
+                    </p>
+                </div>
+
+                <div className="col-12 col-sm-6 col-md-3 text-center mb-4">
+                    <h4>Translated Website</h4>
+                    <p className="edu">
+                        - Expand reach with multilingual support <br />
+                        - Available in Spanish, Dutch, Portuguese, Hindi, and more  
+                    </p>
+                </div>
+
+                <div className="col-12 col-sm-6 col-md-3 text-center mb-4">
+                    <h4>Security Services</h4>
+                    <p className="edu">
+                        - Specialized security for healthcare data & providers <br />
+                        - Security auditing & development to protect sensitive information  
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Services;
