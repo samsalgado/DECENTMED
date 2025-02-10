998baae5916233fcd74407942da5acf52091c882
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-02-09 22:31:10 -0500

    Create dutch.js

---
+++ src/pages/blogs/dutch.js
@@ -0,0 +1,80 @@
+import spanish from '../../images copy/IMG_7054.jpeg';
+import spanish2 from '../../images copy/IMG_7055.jpeg';
+import spanish3 from '../../images copy/IMG_7056.jpeg';
+import spanish4 from '../../images copy/IMG_7057.jpeg';
+import spanish5 from '../../images copy/IMG_7058.jpeg';
+import spanish6 from '../../images copy/IMG_7059.jpeg';
+import spanish7 from '../../images copy/IMG_7060.jpeg';
+import spanish8 from '../../images copy/IMG_7061.jpeg';
+import spanish9 from '../../images copy/IMG_7062.jpeg';
+import spanish10 from '../../images copy/IMG_7063.jpeg';
+import spanish11 from '../../images copy/IMG_7064.jpeg';
+import spanish12 from '../../images copy/IMG_7065.jpeg';
+import spanish13 from '../../images copy/IMG_7066.jpeg';
+import spanish14 from '../../images copy/IMG_7067.jpeg';
+import spanish15 from '../../images copy/IMG_7068.jpeg';
+import spanish16 from '../../images copy/IMG_7069.jpeg';
+import spanish17 from '../../images copy/IMG_7070.jpeg';
+import "../../App.css";
+          const Ivermectinedutch = ({t}) => {
+return {
+<>
+        <div className='container'>
+          <p>
+          <img className='gridimag' src={spanish} alt="logo" />
+        </p>
+           <p>
+           <img className='gridimag' src={spanish2} alt="logo" />
+           </p>
+           <p>
+            <img className='gridimag' src={spanish3} alt="logo" />
+           </p>
+           <p>
+            <img className='gridimag' src={spanish4} alt="pic" />
+           </p>
+           <p>
+           <img className='gridimag' src={spanish5} alt="pic" />
+           </p>
+           <p>
+            <img className='gridimag' src={spanish6} alt="pic" />
+           </p>
+           <p>
+           <img className='gridimag' src={spanish7} alt="pic" />
+           </p>
+           <p>
+           <img className='gridimag' src={spanish8} alt="pic" />
+           </p>
+           <p>
+           <img className='gridimag' src={spanish9} alt="pic" />
+           </p>
+            <p>
+                <img className='gridimag' src={spanish10} alt="pic" />
+            </p>
+            <p>
+                <img className='gridimag' src={spanish11} alt="pic" />
+            </p>
+            <p>
+                <img className='gridimag' src={spanish12} alt="pic" />
+            </p>
+  <p>
+                <img className='gridimag' src={spanish13} alt="pic" />
+            </p>
+
+  <p>
+                <img className='gridimag' src={spanish14} alt="pic" />
+            </p>
+
+  <p>
+                <img className='gridimag' src={spanish15} alt="pic" />
+            </p>
+
+  <p>
+                <img className='gridimag' src={spanish16} alt="pic" />
+            </p>
+
+  <p>
+                <img className='gridimag' src={spanish17} alt="pic" />
+            </p>
+</div>
+
+export default Ivermectinedutch;
