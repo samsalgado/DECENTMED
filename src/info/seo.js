2bb512fa2b586121748df99acc8c3821df70f1b7
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-03-06 14:00:05 -0500

    Create seo.js

---
+++ src/info/seo.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Services from './service.js';
+import './acu.css'; // Import only acu.css for styling
+import { useTranslation } from 'react-i18next';
+
+const SEO = () => {
+const {t} = useTranslation('common')
+  return (
+    <div className='container'>
+      <h1>{t('Digital Marketing Agency')}</h1>
+
+
+      <iframe
+            width="100%"
+            height={315}
+            src= "https://www.youtube.com/embed/_ROUBiWA0MY"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+
+    <Services />
+    </div>
+    
+  )
+}
+
+export default SEO
