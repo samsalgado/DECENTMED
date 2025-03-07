067edbd02ded1d32f1bfc012ab7aead711861c99
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-03-07 20:53:31 -0300

    Update nutritionpg.js

--- src/info/nutritionpg.js
+++ src/info/nutritionpg.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "../App.css";
 import Collapsible from 'react-collapsible';
 import obese from '../images copy/obese.png';
+import './Info.css';
 import drsebi from '../images copy/drsebi.jpeg';
 import { useTranslation } from 'react-i18next';
 
@@ -10,7 +11,7 @@ const Nutritionpage = () => {
 
   return (
     <div className='container'>
-      <h1>{t('Nutrition')}</h1>
+      <h1 className="title">{t('Nutrition')}</h1>
       <iframe width="100%" height="650px" src="https://www.youtube.com/embed/2sZbrRXHJiU?si=kW8BOV3isy3SsQiD" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
 
       <div className="title-container">
