5915c76fa6a53161ab9e96af890e7f6963f06ec4
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-03-07 20:45:21 -0300

    Update chelation.js

--- src/info/chelation.js
+++ src/info/chelation.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../App.css";
+import './Info.css';
 import Collapsible from 'react-collapsible';
 import graphic from '../images copy/chelation101.png';
 import chelationgraphic from '../images copy/chelationgraphic.png';
@@ -11,7 +12,7 @@ const Pgchelation = () => {
 
   return (
     <div className='container'>
-            <h1>{t("Chelation")}</h1> 
+            <h1 className='title'>{t("Chelation")}</h1> 
 
   <iframe width="100%" height="500px" src="https://www.youtube.com/embed/MLNGwB26M5k?si=mC7cdIxG3bRR8RQ9"  title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
   <Button variant="primary" href="http://crossroadsnaturopathic.com/" target="_blank">{t("Visit Crossroads Naturopathic")}</Button>  {/* Added Bootstrap Button */}
