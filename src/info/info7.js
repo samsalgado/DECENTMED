fa4562ffa6393474cc8713a985a7f44c09ff82a1
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-03-07 20:57:46 -0300

    Update info7.js

--- src/info/info7.js
+++ src/info/info7.js
@@ -23,7 +23,7 @@ const Info7 = () => {
         <Services />
       </div>
       <div className='parc'>
-        <h2 style={{ textAlign: 'center', margin: '20px 0' }}>{t('Attention Holistic Practitioners')}</h2>
+        <h2 style={{ textAlign: 'center', margin: '20px 0', fontFamily: 'sans-serif'}}>{t('Attention Holistic Practitioners')}</h2>
 
         <p>
           {t('Welcome to Decent Med: the showcase of Functional Medicine and its revolutionary impact on the lives of billions. Moreover, notice that some of the pages on the website are not robust with information - this is by design. This platform is your canvas. I want beneficial and relevant information from practitioners experienced in the field. This platform gives industry experts a voice to patients globally and grows their practice in a new manner.')}
