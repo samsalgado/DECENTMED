93a7722d78fe39b95a218b62a2fa7dc6e4e34e47
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-03-07 20:43:41 -0300

    Update Herbal.js

--- src/info/Herbal.js
+++ src/info/Herbal.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./homeo.css";
+import './Info.css';
 import homeo from '../images copy/homeo.png';
 import { useTranslation } from 'react-i18next';
 const Herbal = () => {
 const {t} = useTranslation('common')
   return (
     <div className='container'>
-        <h1>{t('Herbal Medicine')}</h1>
+        <h1 className='title'>{t('Herbal Medicine')}</h1>
 
     <img src={homeo} className="HOMEO" alt="logo" />
 <br></br>
