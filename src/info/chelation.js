355ddc216aaaa298cd02bdb48480e05dd0122c43
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-03-07 22:36:13 -0300

    Update chelation.js

--- src/info/chelation.js
+++ src/info/chelation.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import "../App.css";
 import './Info.css';
 import Collapsible from 'react-collapsible';
@@ -6,44 +6,46 @@ import graphic from '../images copy/chelation101.png';
 import chelationgraphic from '../images copy/chelationgraphic.png';
 import chelationbenefits from '../images copy/chelationbenefits.png';
 import { Button } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next';
+
 const Pgchelation = () => {
   const { t } = useTranslation('common');
 
   return (
     <div className='container'>
-            <h1 className='title'>{t("Chelation")}</h1> 
+      <h1 className='title'>{t("Chelation")}</h1>
 
-  <iframe width="100%" height="500px" src="https://www.youtube.com/embed/MLNGwB26M5k?si=mC7cdIxG3bRR8RQ9"  title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-  <Button variant="primary" href="http://crossroadsnaturopathic.com/" target="_blank">{t("Visit Crossroads Naturopathic")}</Button>  {/* Added Bootstrap Button */}
+      {/* YouTube Video */}
+      <iframe
+        width="100%"
+        height="500px"
+        src="https://www.youtube.com/embed/MLNGwB26M5k?si=mC7cdIxG3bRR8RQ9"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
 
-  <div className="title-container">
+      {/* Button Container - Centering and Stacking */}
+      <div className="button-container">
+        <Button variant="primary" href="http://crossroadsnaturopathic.com/" target="_blank">
+          {t("Visit Crossroads Naturopathic")}
+        </Button>
 
-   
-<table className="collapsibles-table">
-    <tbody> 
-    <Collapsible trigger={
-      <Button>
-     {t("Chelation 101 (Heavy Metal Detox)")}
-     </Button>}>
-        <tr>
-          <img alt='' />
-        <img className='chelation-graphic' src={graphic} alt='' />
-        <img className='gridimag' src={chelationgraphic} alt='' />
-        <img className='dual' src={chelationbenefits} alt='' />
-        <img alt='' />
-        </tr>
-      
-      </Collapsible>
+        <Collapsible trigger={
+          <Button variant="secondary">
+            {t("Chelation 101 (Heavy Metal Detox)")}
+          </Button>
+        }>
+          <div className="collapsible-content">
+            <img className='chelation-graphic' src={graphic} alt="Chelation 101" />
+            <img className='gridimag' src={chelationgraphic} alt="Chelation Graphic" />
+            <img className='dual' src={chelationbenefits} alt="Chelation Benefits" />
+          </div>
+        </Collapsible>
+      </div>
 
-    </tbody>
-  </table>
-
-</div>
-
-  </div>
-    
-  )
+    </div>
+  );
 }
 
-export default Pgchelation
+export default Pgchelation;
