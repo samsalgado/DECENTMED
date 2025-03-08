2a7dc030ee3133829a9b5cfe0c3c99c4bec416b3
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-03-07 23:59:15 -0300

    Update cancer.js

--- src/pages/blogs/cancer.js
+++ src/pages/blogs/cancer.js
@@ -23,7 +23,7 @@ const Blog6 = () => {
         <Topbar />
       </header>
       <div className='container'>
-        <h1 style={{ textDecoration: 'underline', textAlign: 'center', margin: '20px 0' }}>{t("Vitamin B: Vitamin B17 - The Mysterious Case of Cancer")}
+        <h1 style={{ textDecoration: 'underline', textAlign: 'center', margin: '20px 0', fontFamily: 'sans-serif' }}>{t("Vitamin B: Vitamin B17 - The Mysterious Case of Cancer")}
         </h1>
         <div className='container'>
           <div className='video-container'>
