2e9ad30e56358ed96dca02a279062922ad00d2cc
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-04-01 13:40:45 -0300

    Update marketingimg.js

--- src/info/marketingimg.js
+++ src/info/marketingimg.js
@@ -1,15 +1,16 @@
 import React from "react";
 import '../App.css';
-import { useTranslation } from 'react-i18next';
-import backgroundImage from '../images copy/background.jpg';
+import backgroundImage from './background.jpg';
 
-const Marketing = () => {
+function Values() {
     const learnmore = () => {
-        window.location.href = 'https://themerlingroupworld.com/digitalmarketing';
+        window.location.href = 'https://themerlingroupworld.com/digitalmarketing'
     }
 
     return (
         <>
+
+            {/* Marketing card section */}
             <div className="marketing-section">
                 <div className="row justify-content-center">
                     <div className="col-12 col-md-6">
@@ -21,12 +22,15 @@ const Marketing = () => {
                                 backgroundPosition: 'center',
                                 border: 'none',
                                 height: '560px',
-                                width: '1000px',
-                                color: 'white',
+                                width:'1000px', // Set a height for the card
+                                color: 'white', // Ensure text is readable against the background
                             }}
                         >
-                            <div className="card-body">
-                                <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '-0.5cm' }}>{'Content Creation'}</p>
+                            <div 
+                                className="card-body" 
+                            >
+        
+                                <p style={{ textAlign: 'right', color:'#FFFF00', marginLeft: '-0.5cm' }}>{'Content Creation'}</p>
                                 <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '-0.5cm' }}>{'Video Editing'}</p>
                                 <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '0.5cm' }}>{'Advertising'}</p>
                                 <p style={{ textAlign: 'right', color: '#FFFF00', marginRight: '-0.7cm' }}>{'Custom Storytelling'}</p>
@@ -34,10 +38,10 @@ const Marketing = () => {
                                 <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '1cm' }}>{'Data Reporting'}</p>
                                 <p style={{ textAlign: 'right', color: '#FFFF00', marginRight: '-0.77cm' }}>{'SEO (Local & National)'}</p>
                                 <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '1cm' }}>{'Webinar Creation'}</p>
-                                <p style={{ textAlign: 'right', color: '#FFFF00', marginRight: '-0.77cm' }}>{'Social Media Optimization'}</p>
+                                <p style={{ textAlign: 'right', color: '#FFFF00', marginRight: '-0.77cm'  }}>{'Social Media Optimization'}</p>
                                 <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '1cm' }}>{'Custom Page Creation'}</p>
                                 <p style={{ textAlign: 'right', color: '#FFFF00', marginRight: '-0.77cm' }}>{'Conversion Optimization'}</p>
-                                <p style={{ textAlign: 'right', color: '#FFFF00', marginRight: '-0.6cm' }}>{'Branding & Consultation'}</p>
+                                <p style={{ textAlign: 'right', color: '#FFFF00', marginRight: '-0.6cm'}}>{'Branding & Consultation'}</p>
                                 <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '1cm' }}>{'Email Marketing'}</p>
                                 <button onClick={learnmore} className="btn btn-primary">{'Learn More'}</button>
                             </div>
@@ -49,4 +53,4 @@ const Marketing = () => {
     );
 }
 
-export default Marketing;
+export default Values;
