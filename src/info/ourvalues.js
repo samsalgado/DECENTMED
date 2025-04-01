f2ecd407db246ea56a2b1ed75e50da8b52d38f81
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-04-01 13:09:44 -0300

    Update ourvalues.js

--- src/info/ourvalues.js
+++ src/info/ourvalues.js
@@ -30,41 +30,7 @@ function Values() {
                 </div>
             </div>
         </div>
-        <div className="marketing-section">
-            <div className="row justify-content-center">
-                <div className="col-12 col-md-6">
-                    <div 
-                        className="card text-center"
-                        style={{ 
-                            backgroundImage: `url(${backgroundImage})`,
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                            border: 'none',
-                            height: '560px',
-                            width:'100%',
-                            color: 'white',
-                        }}
-                    >
-                        <div className="card-body">
-                            <p style={{ textAlign: 'right', color:'#FFFF00', marginLeft: '-0.5cm' }}>{t('Content Creation')}</p>
-                            <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '-0.5cm' }}>{t('Video Editing')}</p>
-                            <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '0.5cm' }}>{t('Advertising')}</p>
-                            <p style={{ textAlign: 'right', color: '#FFFF00', marginRight: '-0.7cm' }}>{t('Custom Storytelling')}</p>
-                            <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '1cm' }}>{t('Automation')}</p>
-                            <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '1cm' }}>{t('Data Reporting')}</p>
-                            <p style={{ textAlign: 'right', color: '#FFFF00', marginRight: '-0.77cm' }}>{t('SEO (Local & National)')}</p>
-                            <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '1cm' }}>{t('Webinar Creation')}</p>
-                            <p style={{ textAlign: 'right', color: '#FFFF00', marginRight: '-0.77cm' }}>{t('Social Media Optimization')}</p>
-                            <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '1cm' }}>{t('Custom Page Creation')}</p>
-                            <p style={{ textAlign: 'right', color: '#FFFF00', marginRight: '-0.77cm' }}>{t('Conversion Optimization')}</p>
-                            <p style={{ textAlign: 'right', color: '#FFFF00', marginRight: '-0.6cm'}}>{t('Branding & Consultation')}</p>
-                            <p style={{ textAlign: 'right', color: '#FFFF00', marginLeft: '1cm' }}>{t('Email Marketing')}</p>
-                            <button onClick={learnmore} className="btn btn-primary">{t('Learn More')}</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+       
         </>
     );
 }
