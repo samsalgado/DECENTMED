cf097aba5f7dc7d44a9bfa41c7810025d94c99b4
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-04-04 16:57:19 -0300

    Update offer.js

--- src/offers/offer.js
+++ src/offers/offer.js
@@ -16,6 +16,7 @@ const { t } = useTranslation("common");
           <img src={imge} className="pik1 bitc" alt=''  />
           <h2>{t('Pay with Bitcoin')}</h2>
           <h3>$250: Including Marketing, Security, Social Media, & Telehealth</h3>
+          <p> BTC Address: bc1q4kkfy6apvfy5maf6c3f24utq925x64t622z20c </p>
           </div>
         <CustomizedDialogs>
           <AddtoCart />
