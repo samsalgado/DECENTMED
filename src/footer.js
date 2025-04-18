8e872d3124fa93d9ce277049f65ecbd78db7c584
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-04-18 15:47:57 -0300

    Update footer.js

--- src/footer.js
+++ src/footer.js
@@ -3,6 +3,7 @@ import LOGO from "./images copy/LOGO.png";
 import Grid from './info/grid';
 import x from './images copy/x.png';
 import youtube from './images copy/youtube.png';
+import geyser from './images copy/geyser.png';
 import './App.css';
 
 const Footer = () => {
@@ -15,6 +16,9 @@ const Footer = () => {
         </a>
         <a href='https://www.youtube.com/playlist?list=PL8Q31etEj6NEEZGvqEw0UAOUG4O0ucsiz'>
             <img src={youtube} alt='Youtube' className='Logo'  />
+        </a>
+     <a href='https://geyser.fund/project/decentmedpubliccancercure/'>
+            <img src={geyser} alt='Geyser Fund' className='Logo'  />
         </a>
         </div>
           <div><img src={LOGO} className="Logo" alt="logo" /></div>
