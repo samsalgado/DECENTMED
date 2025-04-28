1522c1bbe3171637ed4a3e59492a5a17662c67f5
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-04-28 14:48:45 -0300

    Update ans.js

--- src/info/ans.js
+++ src/info/ans.js
@@ -62,7 +62,7 @@ const ANS = () => {
             <Card.Header className='card-header' as="h5">{t("What are your goals?")}</Card.Header>
             <Card.Body>
               <Card.Text>
-                 {t("- showcasing revolutionary treatments to billions of people worldwide")}
+                 {t("- Showcasing revolutionary treatments to billions of people worldwide")}
               </Card.Text>
               <Card.Text>
                 {t("- Aid in global Bitcoin adoption")}
