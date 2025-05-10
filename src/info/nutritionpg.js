b646a0ef3f555ed2dcb177b9608581dcbf366ec2
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-05-10 18:24:39 -0300

    Update nutritionpg.js

--- src/info/nutritionpg.js
+++ src/info/nutritionpg.js
@@ -5,6 +5,16 @@ import obese from '../images copy/obese.png';
 import labs from '../images copy/8.jpg';
 import brain from '../images copy/9.jpg';
 import './Info.css';
+import drSebiES '../images copy/IMG_9273.jpeg';
+import NUTES '../images copy/IMG_9306.jpeg';
+import NUTFR '../images copy/IMG_9307.jpeg';
+import NUTNL '../images copy/IMG_9308.jpeg';
+import NUTHindi '../images copy/IMG_9309.jpeg';
+import drSebiHindi '../images copy/IMG_9274.jpeg';
+import drSebINT '../images copy/IMG_9275.jpeg';
+import drSebiFrench '../images copy/IMG_9276.jpeg';
+import cogES '../images copy/IMG_9277.jpeg'; 
+import cogFR '../images copy/IMG_9278.jpeg'; 
 import drsebi from '../images copy/drsebi.jpeg';
 import { useTranslation } from 'react-i18next';
 
@@ -30,6 +40,10 @@ const Nutritionpage = () => {
               <tr>
                 <img alt='' />
                 <img className='gridimage' src={drsebi} alt='' />
+                <img className='gridimage' src={drSebiFrench} alt='' />
+                <img className='gridimage' src={drSebINT} alt='' />
+                <img className='gridimage' src={drSebiHindi} alt='' />
+                <img className='gridimage' src={drsebiES} alt='' />
                 <img alt='' />
               </tr>
             </Collapsible>
@@ -37,6 +51,14 @@ const Nutritionpage = () => {
               <div className="collapsible-content">
                 <img className='griddimage' src={labs} alt="Functional Nutrition Resources"/>
                  <img className='griddimage' src={brain} alt="Functional Nutrition Resources"/>
+                <img className='griddimage' src={cogHindi} alt="Functional Nutrition Resources"/>
+                <img className='griddimage' src={cogDutch} alt="Functional Nutrition Resources"/>
+                <img className='griddimage' src={cogFR} alt="Functional Nutrition Resources"/>
+                <img className='griddimage' src={cogES} alt="Functional Nutrition Resources"/>
+                <img className='griddimage' src={NUTHindi} alt="Functional Nutrition Resources"/>
+                <img className='griddimage' src={NUTNL} alt="Functional Nutrition Resources"/>
+                <img className='griddimage' src={NUTFR} alt="Functional Nutrition Resources"/>
+                <img className='griddimage' src={NUTES} alt="Functional Nutrition Resources"/>
     <img alt='' />
               </div>
             </Collapsible>                   
