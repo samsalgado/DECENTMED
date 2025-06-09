02ebd5dc264473ef9599e25edf3af6c069d0b555
samsalgado <samueljuansalgado@gmail.com>
2025-06-08 23:13:52 -0300

    update

--- src/info/reviews.js
+++ src/info/reviews.js
@@ -9,6 +9,7 @@ import graphic3 from '../images copy/IMG_7979.jpeg';
 import graphic4 from '../images copy/IMG_7980.jpeg';
 import graphic5 from '../images copy/IMG_7981.jpeg';
 import graphic6 from '../images copy/IMG_7977.jpeg';
+import newreview from '../images copy/newreview.png';
 import lori from '../images copy/IMG_9733.jpg';
 import { useTranslation } from 'react-i18next';
 
@@ -26,6 +27,7 @@ const Reviews = () => {
       }
     >
             <img className="gridimag" src={lori} alt="Bone Marrow Stem Cell Benefits" />
+            <img className="gridimag" src={newreview} alt="Testimonial" />
             <img className="gridimag" src={graphic2} alt="Bone Marrow Stem Cell Benefits" />
             <img className="gridimag" src={graphic3} alt="Bone Marrow Stem Cell Benefits" />
             <img className="gridimag" src={graphic4} alt="Bone Marrow Stem Cell Benefits" />
