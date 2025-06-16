f027e78b3b2a0b71e507b3feacfe2c68e4ee3916
samsalgado <samueljuansalgado@gmail.com>
2025-06-16 15:34:08 -0300

    update

--- src/info/practices.js
+++ src/info/practices.js
@@ -31,7 +31,7 @@ function Practices() {
                     </div>
                     <div className="col-12 col-sm-6 col-md-4 col-lg-3 text-center mb-4">
                     <NavLink to="/coaching">
-                        <h3 className="card-title-fixed" >{t("Health Coach")}</h3>
+                        <h3 className="card-title-fixed" >{t("Wellness Coach")}</h3>
                         <img className="img-custom" src={Departments.healthcoach} alt={t("Health Coach")} />
                     </NavLink>
                     </div>
