e7bb94538f25089d4243bfba444223b12a864797
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2025-07-15 14:58:23 -0600

    Update coach.js

--- src/pages/coach.js
+++ src/pages/coach.js
@@ -2,13 +2,14 @@ import '../App.css';
 import { Helmet } from 'react-helmet'; // Import Helmet
 import Topbar from './topbar';
 import Footer from '../footer';
+import Ramona from '../cards/IMG_0587.jpeg';
 import Gorilla from '../cards/gorilla.png';
 import { useTranslation } from 'react-i18next'
 export function Coaching() {
 const { t } = useTranslation('common');
     return(
         <div>
-                          <main className="page-content">
+        <main className="page-content">
 
         <Helmet>  {/* Add Helmet component */}
         <title>{t("Health Coaching Near Me")}</title>  
@@ -55,6 +56,24 @@ const { t } = useTranslation('common');
   <h5>{t("Gorilla and She Coaching:")}</h5>
       <p>
         {t("gorilla")}
+      </p>
+    </div>
+  </div>
+<div class="row">
+    <div class="col-md-6">
+      <h3>{t("Authentic Life Journey")}</h3>
+      <a href='https://authenticlifejourney.com/'>
+    <figure>
+        <img src={Ramona} alt="logo" />
+          <figcaption>{t("View Website")}</figcaption>
+        </figure>
+      </a>
+     
+  <h5>{t("Ramona Crabtree-Falkner: Transformational Empowerment Coach")}</h5>
+      <p>
+        {t("I see you. You’re a dedicated, high-achieving professional woman who seems to have it all together on the outside, yet inside, you feel exhausted, overwhelmed, and disconnected. You’re juggling responsibilities, meeting expectations, and giving your all to your career, family, and the world—yet you feel like something is missing. I know this struggle well because I’ve been exactly where you are. I spent years hustling—building a business, pushing through exhaustion, and ignoring what my body and spirit needed—until it all caught up with me. Burnout, health challenges, and a deep sense of disconnect forced me to stop and reassess. I had to learn to prioritize myself, set boundaries, and embrace aligned action instead of constantly striving.
+Now, I live what I teach. I choose balance, make space for joy, and trust my intuition to guide me forward. And I help women like you do the same.
+")}
       </p>
     </div>
   </div>
