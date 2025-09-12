6c96d76c5f498bf7f96ee72a266fd759f98bd592
samsalgado <samueljuansalgado@gmail.com>
2025-09-12 15:37:52 -0600

    swird

--- src/pages/breathworkers.js
+++ src/pages/breathworkers.js
@@ -1,75 +1,3 @@
-
-// import React from 'react';
-// import '../App.css';
-// import { Helmet } from 'react-helmet'; // Import Helmet
-// import Topbar from './topbar';
-// import Footer from '../footer';
-// //import ReactPlayer from 'react-player';
-// import JenB from '../cards/scarlett.JPG';
-// import { useTranslation } from 'react-i18next'
-
-// export function Breathwork() {
-//   const { t } = useTranslation('common');
-
-
-//   return (
-//     <div>
-//       <main className="page-content">
-//         <Helmet>
-//           <title>{t("Somatic Breathwork")}</title>
-//           <meta name="description" content={t("If you want one of the best emotional eating books...")} />
-//         </Helmet>
-
-//         <header>
-//           <Topbar />
-//         </header>
-
-//         <div className="container">
-//           <h1>{t("Somatic Breathwork & Breathwork Coaches")}</h1>
-
-//           <div className="row">
-//             <div className="col-md-6">
-//               <h2>{t("Scarlett Dee")}</h2>
-//               <figure>
-//                 <img src={JenB} alt="Scarlett" />
-//               </figure> <br />
-
-//               <a
-//                 href="mailto:info@tribe-yoga.com"
-//                 className="custom-btn"                style={{
-//                   display: "inline-block",
-//                   padding: "10px 20px",
-//                   background: "#6c63ff",
-//                   color: "#fff",
-//                   borderRadius: "8px",
-//                   textDecoration: "none",
-//                   marginTop: "10px",
-//                 }}
-//               >
-//                 {t("Email Scarlett")}
-//               </a>
-
-//               <h3>{t("Scartlett Dee, Somatic Breathwork Coach:")}</h3>
-//               <p>
-//                 {t("Scarlett has over 30 years of experience in yoga, healing, and self-mastery. She guides transformational breathwork journeys that help release stored trauma, regulate the nervous system, and support deep emotional healing. Her sessions offer a powerful reset—reducing stress, calming anxiety, and rebuilding confidence from the inside out. Scarlett’s work is both grounding and expansive, helping clients overcome inner challenges and return to a state of clarity, presence, and aligned self-leadership.")}
-//               </p>
-//               <p>
-//                 {t("Scarlett is a certified: 9D Breathwork Coach & Facilitator, Hypnotherapist, Holistic Therapist, Tantra & Yoga Teacher Trainer")}
-//               </p>
-//             </div>
-//           </div>
-//         </div>
-
-//         <footer>
-//           <Footer />
-//         </footer>
-//       </main>
-//     </div>
-//   );
-// }
-
-
-
 import '../App.css';
 import { Helmet } from 'react-helmet';
 import Topbar from './topbar';
@@ -78,8 +6,9 @@ import { useEffect, useState } from 'react';
 //import ReactPlayer from 'react-player';
 import JenB from '../cards/scarlett.JPG';
 import { useTranslation } from 'react-i18next';
-import PhoneInput from 'react-phone-input-2'; // ✅ Import
+//import PhoneInput from 'react-phone-input-2'; // ✅ Import
 import axios from 'axios';
+
 export function Breathwork({ providerEmail }) {
 
   useEffect(() => {
@@ -90,13 +19,14 @@ export function Breathwork({ providerEmail }) {
   const [formData, setFormData] = useState({ name: '', email: '', phone: '' });
   const [status, setStatus] = useState('');
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  //const handleChange = (e) => {
+    //setFormData({ ...formData, [e.target.name]: e.target.value });
+ // };
 
-  const handlePhoneChange = (value) => {
+  /*const handlePhoneChange = (value) => {
     setFormData({ ...formData, phone: value });
   };
+  */
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -104,7 +34,6 @@ export function Breathwork({ providerEmail }) {
 
     try {
       await axios.post("https://decentmed-server.vercel.app/send-email", {
-        // await axios.post("http://localhost:5000/send-email", {
         ...formData,
         providerEmail,
       });
@@ -115,8 +44,7 @@ export function Breathwork({ providerEmail }) {
       setStatus("Failed to send email");
     }
   };
-
-
+console.log(setFormData);
   return (
     <div>
       <main className="page-content">
@@ -139,56 +67,24 @@ export function Breathwork({ providerEmail }) {
                 <img src={JenB} alt="Scarlett" />
               </figure>
               <br />
-              <figcaption className="scarle">{t("Contact Scarlett Dee")}</figcaption>
 
               {/* Contact Form */}
               <form onSubmit={handleSubmit} className="contact-form">
-                <div>
-                  <label>Name</label>
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="Enter your name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-                <div>
-                  <label>Email</label>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Enter your email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-                <div>
-                  <label>Phone</label>
-                  <PhoneInput
-                    country={'us'}
-                    value={formData.phone}
-                    onChange={handlePhoneChange}
-                    inputStyle={{
-                      width: '100%',
-                      height: '50px',       // <-- normal input height-er moto
-                      fontSize: '16px',     // optional
-                    }}
-                    buttonStyle={{
-                      height: '50px',       // flag button height match
-                    }}
-                  />
-                </div>
-                <button style={{
-                  backgroundColor: "#00695C",  color: "#fff",  border: "none", boxShadow: "none", outline: "none"
-                }}
-                  type="submit">Submit</button>
+              <div style={{ marginTop: "20px" }}>
+                <iframe
+                  src="https://www.cognitoforms.com/f/QLZd_Lg6oEmOhHz0oQSkVQ/1"
+                  allow="payment"
+                  style={{ border: 0, width: "100%" }}
+                  height="600"
+                  title="Cognito Form"
+                  required
+                ></iframe>
+                <script src="https://www.cognitoforms.com/f/iframe.js"></script>
+              </div>
               </form>
               <p>{status}</p>
 
-              <h5>{t("Scartlett Dee, Somatic Breathwork Coach:")}</h5>
+              <h5>{t("Scarlett Dee, Somatic Breathwork Coach:")}</h5>
               <p>
                 {t(
                   "Scarlett has over 30 years of experience in yoga, healing, and self-mastery. She guides transformational breathwork journeys that help release stored trauma, regulate the nervous system, and support deep emotional healing. Her sessions offer a powerful reset—reducing stress, calming anxiety, and rebuilding confidence from the inside out. Scarlett’s work is both grounding and expansive, helping clients overcome inner challenges and return to a state of clarity, presence, and aligned self-leadership."
