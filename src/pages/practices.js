d7abcab33df69ca7fd4a4bc40aab6e8a05ea2971
samsalgado <samueljuansalgado@gmail.com>
2025-09-24 15:44:48 -0600

    uwwu

--- src/pages/practices.js
+++ src/pages/practices.js
@@ -17,9 +17,7 @@ const Practice = () => {
 
         <div className="signup-container">
           <h1 className="signup">{t('Sign Up')}</h1>
-          <iframe class="rumble" title="DecentMed Explained"         allowFullScreen
- width="640" height="360" src="https://rumble.com/embed/v6wu76s/?pub=4hu51y" frameborder="0" allowfullscreen></iframe>
-  </div>
+<iframe class="rumble" title="signup" width="640" height="360" src="https://rumble.com/embed/v6x8bpu/?pub=4hu51y" frameborder="0" allowfullscreen></iframe>  </div>
         
         <div style={{ display: "flex", justifyContent: "center", marginTop: "1rem" }}>
           <Button className="custom-btn" href="https://cal.com/merlin-ayx5zg/30min?overlayCalendar=true" target="_blank">
