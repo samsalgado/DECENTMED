f5324b5bbe061577f5b8a930d78453670caf88a4
samsalgado <samueljuansalgado@gmail.com>
2025-12-05 01:27:28 -0500

    f

--- src/info/infos.js
+++ src/info/infos.js
@@ -1,11 +1,13 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './Info.css';
 import Speakers from './summitspeakers';
 import { useTranslation } from 'react-i18next'; // Import useTranslation hook
 import summit from "../images copy/decentmedsummit.png";
 import { Button } from 'react-bootstrap';
+
 const InfoSummit = () => {
   const { t } = useTranslation("common"); // Access translation function
+  
   useEffect(() => {
     // Load GHL script once
     const script = document.createElement("script");
@@ -36,8 +38,9 @@ const InfoSummit = () => {
         </div>
       </div>
       <Speakers />
-      <div style={{ width: "100%", maxWidth: "600px", margin: "0 auto" }}>
-        <h2 style={{ textAlign: "center", marginBottom: "-30px" }}>
+      
+      <div style={{ width: "100%", maxWidth: "600px", margin: "20px auto" }}>
+        <h2 style={{ textAlign: "center", marginBottom: "20px" }}>
           {t("Register")}
         </h2>
       </div>
@@ -46,9 +49,10 @@ const InfoSummit = () => {
         src="https://api.leadconnectorhq.com/widget/form/hbb1megFlHaUKjxVNheq"
         style={{
           width: "100%",
-          height: "100%",
+          height: "773px", // Explicit height for the iframe
           border: "none",
-          borderRadius: "3px"
+          borderRadius: "3px",
+          marginTop: "20px" // Space between the heading and iframe
         }}
         id="inline-hbb1megFlHaUKjxVNheq"
         data-layout="{'id':'INLINE'}"
@@ -59,7 +63,6 @@ const InfoSummit = () => {
         data-deactivation-type="neverDeactivate"
         data-deactivation-value=""
         data-form-name="Summit Reg"
-        data-height="773"
         data-layout-iframe-id="inline-hbb1megFlHaUKjxVNheq"
         data-form-id="hbb1megFlHaUKjxVNheq"
         title="Summit Reg"
