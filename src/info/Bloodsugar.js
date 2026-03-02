d7ce6d7fab224ed3315630065164a0c91834d6f1
samsalgado <samueljuansalgado@gmail.com>
2026-03-02 10:43:42 -0300

    seo push

--- src/info/Bloodsugar.js
+++ src/info/Bloodsugar.js
@@ -7,7 +7,6 @@ import caseE from '../images copy/v5.png';
 import caseF from '../images copy/v6.png';
 import caseH from '../images copy/v7.png';
 import case8 from '../images copy/v8.png';
-import case9 from '../images copy/v8.png';
 import case10 from '../images copy/v9.png';
 import case11 from '../images copy/v10.png';
 import case12 from '../images copy/v11.png';
@@ -19,7 +18,7 @@ import case16 from '../images copy/v15.png';
 import "../App.css";
 const BloodSugar = ({ t }) => {
   const images = [
-    caseA, caseB, caseC, caseD, caseE, caseF, caseH, case8,case9,case10,case11,case12,case13,case14,case15,case16
+    caseA, caseB, caseC, caseD, caseE, caseF, caseH, case8,case10,case11,case12,case13,case14,case15,case16
   ];
   return (
     <div className="container">
