a59b5524df0d073d6a69a5eab8e8806fd93609af
sabbir0070 <mdsharifulalamsiddique79672@gmail.com>
2026-07-01 04:49:39 +0600

    Optimize page weight

--- src/Shared/Footers.js
+++ src/Shared/Footers.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import './Footer.css';
 import { useTranslation } from 'react-i18next';
 import { FaLinkedin, FaWhatsapp, FaYoutube, FaFacebook } from 'react-icons/fa';
@@ -10,6 +10,7 @@ import alignable from "../images copy/alignable.png";
 import wish from "../images copy/wish.png";
 const Footers = () => {
   const { t } = useTranslation('common');
+  const [playVideo, setPlayVideo] = useState(false);
   return (
     <footer className="footer">
       <div className="footer-container">
@@ -98,7 +99,58 @@ const Footers = () => {
             <img src={Inspire} className="geyserImgLarge" alt="Support DecentMed on Geyser Fund" />
             <Button className="custom-btn">Support our Charity of Choice</Button>
           </a>
-          <iframe width="360" height="215" src="https://www.youtube.com/embed/LFTzCOn59_s?si=_FcEzoSIY3wmTOyI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          {playVideo ? (
+            <iframe 
+              width="100%" 
+              style={{ maxWidth: '360px' }}
+              height="215" 
+              src="https://www.youtube.com/embed/LFTzCOn59_s?autoplay=1&si=_FcEzoSIY3wmTOyI" 
+              title="YouTube video player" 
+              frameborder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              referrerpolicy="strict-origin-when-cross-origin" 
+              allowfullscreen
+            ></iframe>
+          ) : (
+            <div 
+              onClick={() => setPlayVideo(true)} 
+              style={{ 
+                cursor: 'pointer', 
+                position: 'relative', 
+                width: '100%', 
+                maxWidth: '360px',
+                height: '215px', 
+                backgroundColor: '#000',
+                borderRadius: '8px',
+                overflow: 'hidden',
+                marginTop: '15px'
+              }}
+            >
+              <img 
+                src="https://img.youtube.com/vi/LFTzCOn59_s/0.jpg" 
+                alt="Video Thumbnail" 
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
+              />
+              <div style={{ 
+                position: 'absolute', 
+                top: '50%', 
+                left: '50%', 
+                transform: 'translate(-50%, -50%)', 
+                backgroundColor: 'rgba(0,105,92,0.85)', 
+                borderRadius: '50%', 
+                width: '50px', 
+                height: '50px', 
+                display: 'flex', 
+                alignItems: 'center', 
+                justifyContent: 'center',
+                boxShadow: '0 4px 10px rgba(0,0,0,0.3)'
+              }}>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="white">
+                  <path d="M8 5v14l11-7z"/>
+                </svg>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="footer-bottom">
--- src/info/wellness.js
+++ src/info/wellness.js
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../info/Info.css'; // Import the CSS file
 import { useTranslation } from 'react-i18next';
 import { Button } from 'react-bootstrap';
 //import x from "../images copy/1000.png" // Import the translation hook
 const WellnessInro = () => {
   const { t } = useTranslation("common"); 
+  const [playVideo, setPlayVideo] = useState(false);
   const learnmore = () => {
     window.location.href = 'https://decentmed.org/signup/provider'
   }
   return (
     <div className='theecontainer'>
       <div className='video-container'>
-      <iframe
-      width="100%"
-      height="215"
-      src="https://www.youtube.com/embed/-osX1InXYYU?si=_pBgh8RtWy_GxXE9"
-      title="Video player"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
-      allowFullScreen
-    ></iframe>
+        {playVideo ? (
+          <iframe
+            width="100%"
+            height="215"
+            src="https://www.youtube.com/embed/-osX1InXYYU?autoplay=1&si=_pBgh8RtWy_GxXE9"
+            title="Video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
+            allowFullScreen
+          ></iframe>
+        ) : (
+          <div 
+            onClick={() => setPlayVideo(true)} 
+            style={{ 
+              cursor: 'pointer', 
+              position: 'relative', 
+              width: '100%', 
+              height: '215px', 
+              backgroundColor: '#000',
+              borderRadius: '8px',
+              overflow: 'hidden'
+            }}
+          >
+            <img 
+              src="https://img.youtube.com/vi/-osX1InXYYU/0.jpg" 
+              alt="Video Thumbnail" 
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
+            />
+            <div style={{ 
+              position: 'absolute', 
+              top: '50%', 
+              left: '50%', 
+              transform: 'translate(-50%, -50%)', 
+              backgroundColor: 'rgba(0,105,92,0.85)', 
+              borderRadius: '50%', 
+              width: '60px', 
+              height: '60px', 
+              display: 'flex', 
+              alignItems: 'center', 
+              justifyContent: 'center',
+              boxShadow: '0 4px 10px rgba(0,0,0,0.3)'
+            }}>
+              <svg width="28" height="28" viewBox="0 0 24 24" fill="white">
+                <path d="M8 5v14l11-7z"/>
+              </svg>
+            </div>
+          </div>
+        )}
       </div>
       <Button onClick={learnmore} target='_blank' className="custom-btn">
           {t('Providers: Join Trusted Network')}
